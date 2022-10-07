c7f009064ee0f53fe1254df21194a99768608be2
Nensi.Skenderi <nensi.skenderi@beamtrail.com>
2022-10-07 17:21:13 +0400

    Customizing user detail queries

--- src/main/java/com/taco/cloud/config/security/SecurityConfig.java
+++ src/main/java/com/taco/cloud/config/security/SecurityConfig.java
@@ -1,14 +1,23 @@
 package com.taco.cloud.config.security;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    DataSource dataSource;
+
+    SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -17,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
          */
 
 
-        // In-memory
+        // In-memory user store
         auth.inMemoryAuthentication()
                     .withUser("nensi")
                     .password("nensi")
@@ -27,6 +36,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .password("test")
                     .authorities("ROLE_USER");
 
-        super.configure(auth);
+        // JDBC user store
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username, password, enabled from Users" + //Customizing user detail queries
+                        "where username = ?")
+                .authoritiesByUsernameQuery("select username, authority from UserAuthorities" +
+                        "where username = ?")
     }
 }
